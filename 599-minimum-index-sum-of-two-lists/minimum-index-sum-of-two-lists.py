6c1fb4aca385b23fe32ffa238225c75489eb69c0
Bisrat G <bisratgulelat@gmail.com>
2026-02-12 03:36:17 +0300

    Time: 70 ms (34.09%) | Memory: 19.7 MB (45.37%) - LeetSync

--- 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
+++ 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.py
@@ -1,19 +1,47 @@
 class Solution:
     def findRestaurant(self, list1: List[str], list2: List[str]) -> List[str]:
+
+        mp = {}
+        sumi = 2001
+        ans = []
+       
+        for i , v in enumerate(list1):
+            mp[v] =i
+
+        for i in range(len(list2)):
+            if list2[i] in list1:
+                if mp[list2[i]]+ i < sumi:
+                    ans=[]
+                    ans.append(list2[i])
+                    sumi = mp[list2[i]]+i
+                elif  mp[list2[i]]+ i == sumi:
+                    ans.append(list2[i])
+        return ans
+
+
+
         
 
-        sumi = 2001
-        result = []
-        for i in range(len(list1)):
-            for j in range(len(list2)):
-                if list1[i]==list2[j]:
-                    if i+j < sumi:
-                        sumi = i+j
-                        result = []
-                        result.append(list1[i])
-                    elif i+j == sumi:
-                        result.append(list1[i])
-        return result 
+        
+
+        
+        
+        
+        
+        
+        
+        # sumi = 2001
+        # result = []
+        # for i in range(len(list1)):
+        #     for j in range(len(list2)):
+        #         if list1[i]==list2[j]:
+        #             if i+j < sumi:
+        #                 sumi = i+j
+        #                 result = []
+        #                 result.append(list1[i])
+        #             elif i+j == sumi:
+        #                 result.append(list1[i])
+        # return result 
 
 
         
