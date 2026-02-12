35ad3e76c889d520fd4a780b710b576d5469d6a7
Bisrat G <bisratgulelat@gmail.com>
2026-02-12 03:02:52 +0300

    Time: 0 ms (100.00%) | Memory: 19.5 MB (26.18%) - LeetSync

--- 722-remove-comments/remove-comments.py
+++ 722-remove-comments/remove-comments.py
@@ -1,64 +1,39 @@
 class Solution:
     def removeComments(self, source: List[str]) -> List[str]:
         
-        result = []
-        in_block = False
-        curr = ""
-
-        for line in source:
-            i = 0
-            while i < len(line):
-                if in_block:
-                    if i + 1 < len(line) and line[i] == '*' and line[i + 1] == '/':
-                        in_block = False
-                        i += 1 
-                else:                   
-                    if i + 1 < len(line) and line[i] == '/' and line[i + 1] == '/':
-                        break
-                    
-                    elif i + 1 < len(line) and line[i] == '/' and line[i + 1] == '*':
-                        in_block = True
-                        i += 1 
-                    else:
-                        curr += line[i]
-                i += 1
-
-            if not in_block and curr:
-                result.append(curr)
-                curr = ""
-
-        return result
-        
-        
-        
-        
-        
-        # state = False 
        
-        # ans = []
         
-        # for i in range(len(source)):
+        
+        
+        
+        
+        state = False 
+        ans = []
+        strs= ""
+        
+        for line in source:
+            j=0
+            while (j < len(line)):
+                if state:
+                    if j+1<len(line) and line[j] == "*"  and line[j+1]== "/":
+                        state=False 
+                        j+=1
 
-        #     j=0
-        #     if state == False:
-        #         strs=""
-        #     while (j < len(source[i])):
-        #         if j<(len(source[i])-1) and source[i][j] == "/"  and source[i][j+1] == "/" and state == False:
-        #             break
-        #         if j<(len(source[i])-1) and source[i][j] == "/"  and source[i][j+1] == "*" and state==False :
-        #             state=True
-        #             j+=2
-        #         if j<(len(source[i])-1) and source[i][j] == "*"  and source[i][j+1] == "/" and state == True:
-        #             state=False 
-        #             j+=2
-                    
-        #         if j<(len(source[i])) and  state == False:
-        #             strs+=source[i][j]
-        #         j+=1
-        #     if state == False and strs!="":
-        #         ans.append(strs)     
-        # source = ans 
-        # return source
+                else:
+                    if j+1<len(line) and line[j]== "/"  and line[j+1] == "/":
+                        break
+                    elif j+1<len(line) and  line[j]== "/" and line[j+1] == "*":
+                        state=True
+                        j+=1
+                    else:
+                        strs +=line[j]           
+                   
+                j+=1
+            if state == False and strs!="":
+                ans.append(strs)   
+                strs= ""
+        
+        return ans
             
 
 
