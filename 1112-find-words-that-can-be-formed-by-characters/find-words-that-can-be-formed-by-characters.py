c807c32c49423aae166c19a19d9476e97a9b2912
Bisrat G <bisratgulelat@gmail.com>
2026-02-13 05:46:17 +0300

    Time: 103 ms (20.79%) | Memory: 19.8 MB (46.57%) - LeetSync

--- 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.py
+++ 1112-find-words-that-can-be-formed-by-characters/find-words-that-can-be-formed-by-characters.py
@@ -1,24 +1,44 @@
 class Solution:
     from collections import Counter
     def countCharacters(self, words: List[str], chars: str) -> int:
-        mp = Counter(chars)
-        count = 0   
-        nowmp = {}     
-
-        for line in words:
-            nowmp.clear()
-            nowmp = Counter(line)
-            b = True 
-            counter= 0 
-            for i in range(len(line)):
-                if mp[line[i]] <nowmp[line[i]]:
-                    b=False
-                    break
-                else:
-                    counter+=1
+        
+        sumLength = 0
+        for word in words:
+            b= True
+            for ch in word:
+                n = word.count(ch)
+                m = chars.count(ch)
+                if n > m:
+                    b = False
             if b:
-                count+=counter
-        return count
+                sumLength += len(word)
+        return sumLength
+            
+
+        
+        
+        
+        
+        
+        
+        # mp = Counter(chars)
+        # count = 0   
+        # nowmp = {}     
+
+        # for line in words:
+        #     nowmp.clear()
+        #     nowmp = Counter(line)
+        #     b = True 
+        #     counter= 0 
+        #     for i in range(len(line)):
+        #         if mp[line[i]] <nowmp[line[i]]:
+        #             b=False
+        #             break
+        #         else:
+        #             counter+=1
+        #     if b:
+        #         count+=counter
+        # return count
 
 
 
