4dc20953a3a3d6acf84ed0e6feabd600023225a0
Bisrat G <bisratgulelat@gmail.com>
2026-02-15 02:51:15 +0300

    Time: 3 ms (62.58%) | Memory: 21.2 MB (46.40%) - LeetSync

--- 136-single-number/single-number.py
+++ 136-single-number/single-number.py
@@ -1,15 +1,21 @@
 class Solution:
     def singleNumber(self, nums: List[int]) -> int:
+        
+        res = 0
+        for i in nums:
+            res^=i
+        return res
 
-        map = {}
-        for i in range(len(nums)):
-            if nums[i] not in map:
-                map[nums[i]] = 0
-            map[nums[i]]+=1
+
+        # map = {}
+        # for i in range(len(nums)):
+        #     if nums[i] not in map:
+        #         map[nums[i]] = 0
+        #     map[nums[i]]+=1
 
                 
-        for i in range(len(nums)):
-            if map[nums[i]]==1: return nums[i]
+        # for i in range(len(nums)):
+        #     if map[nums[i]]==1: return nums[i]
         
         
 
