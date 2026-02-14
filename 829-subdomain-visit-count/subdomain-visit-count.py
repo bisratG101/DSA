30b009e8d9967e5f92a84d48ce330d41eff77407
Bisrat G <bisratgulelat@gmail.com>
2026-02-15 01:12:09 +0300

    Time: 0 ms (100.00%) | Memory: 19.5 MB (25.48%) - LeetSync

--- 829-subdomain-visit-count/subdomain-visit-count.py
+++ 829-subdomain-visit-count/subdomain-visit-count.py
@@ -1,24 +1,28 @@
 class Solution:
     from collections import defaultdict
     def subdomainVisits(self, cpdomains: List[str]) -> List[str]:
+
+        mp = {}
+        ans = []
+        for site in cpdomains:
+            web= site.split(" ")
+            num = int(web[0])
+            sites=web[1]
+            webs=sites.split(".")
+            cp = ""
+            for i in range(len(webs)-1,-1,-1):
+                if i != len(webs)-1:
+                    cp=  webs[i]+"."+cp
+                else:
+                    cp= webs[i]
+                mp[cp] = mp.get(cp,0)+num
+        for key , value in mp.items():
+            strs = str(value)+" "+key
+            ans.append(strs)
+        return ans
+
+
+
     
         
-        count_map = defaultdict(int)
         
-        for entry in cpdomains:
-            count_str, domain = entry.split()
-            count = int(count_str)
-            
-            parts = domain.split('.')
-            
-           
-            for i in range(len(parts)):
-                subdomain = ".".join(parts[i:])
-                count_map[subdomain] += count
-        
-      
-        result = []
-        for domain, total in count_map.items():
-            result.append(f"{total} {domain}")
-        
-        return result
