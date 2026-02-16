2c12ef3a0a15671da7581110ff566761fbb34816
Bisrat G <bisratgulelat@gmail.com>
2026-02-17 02:27:36 +0300

    Time: 112 ms (59.81%) | Memory: 74.6 MB (76.32%) - LeetSync

--- 2778-frequency-tracker/frequency-tracker.py
+++ 2778-frequency-tracker/frequency-tracker.py
@@ -1,38 +1,32 @@
 class FrequencyTracker:
 
     def __init__(self):
-        self.numCount = {}
-        self.freqCount = {}
-
-    def add(self, number: int) -> None:
-        oldFreq = self.numCount.get(number, 0)
-        newFreq = oldFreq + 1
-
-        self.numCount[number] = newFreq
-
-        if oldFreq > 0:
-            self.freqCount[oldFreq] -= 1
-
-        self.freqCount[newFreq] = self.freqCount.get(newFreq, 0) + 1
+        self.mp_count = {}
+        self.mp_ferq = {}     
+       
+    def add(self, number: int) -> None: 
+        old = self.mp_count.get(number,0)  
+        
+        new = old+1
+        self.mp_count[number] = new
+        if old>0:
+            self.mp_ferq[old]-=1
+        self.mp_ferq[new]=self.mp_ferq.get(new,0)+1
 
     def deleteOne(self, number: int) -> None:
-        if number not in self.numCount or self.numCount[number] == 0:
-            return
-
-        oldFreq = self.numCount[number]
-        newFreq = oldFreq - 1
-
-        self.freqCount[oldFreq] -= 1
-
-        if newFreq > 0:
-            self.numCount[number] = newFreq
-            self.freqCount[newFreq] = self.freqCount.get(newFreq, 0) + 1
-        else:
-            del self.numCount[number]
-
+        if number not in self.mp_count or self.mp_count[number]==0:
+            return 
+        old = self.mp_count[number]
+        new = old - 1
+        self.mp_count[number] = new
+        self.mp_ferq[old] -=1
+        if new > 0:
+            self.mp_ferq[new]=self.mp_ferq.get(new , 0)+1
+      
     def hasFrequency(self, frequency: int) -> bool:
-        return self.freqCount.get(frequency, 0) > 0
+        return self.mp_ferq.get(frequency,0)> 0
 
+        
 
 
 # Your FrequencyTracker object will be instantiated and called as such:
