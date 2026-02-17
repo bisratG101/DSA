99f6c0a2a03c47e97c75b4df52c80787c9e0cb3a
Bisrat G <bisratgulelat@gmail.com>
2026-02-17 03:14:08 +0300

    Time: 86 ms (95.64%) | Memory: 79.1 MB (20.56%) - LeetSync

--- 2778-frequency-tracker/frequency-tracker.py
+++ 2778-frequency-tracker/frequency-tracker.py
@@ -2,33 +2,55 @@ class FrequencyTracker:
 
     def __init__(self):
         self.mp_count = {}
-        self.mp_ferq = {}     
+        self.mp_freq = {}     
        
     def add(self, number: int) -> None:
         if number not in self.mp_count:
             self.mp_count[number]=1
-            self.mp_ferq[1]=self.mp_ferq.get(1,0)+1
+            self.mp_freq[1]=self.mp_freq.get(1,0)+1
             return
         old = self.mp_count.get(number,0)  
         
         new = old+1
         self.mp_count[number] = new
         if old>0:
-            self.mp_ferq[old]-=1
-        self.mp_ferq[new]=self.mp_ferq.get(new,0)+1
+            self.mp_freq[old]-=1
+        self.mp_freq[new]=self.mp_freq.get(new,0)+1
 
     def deleteOne(self, number: int) -> None:
-        if number not in self.mp_count or self.mp_count[number]==0:
+        if number not in self.mp_count:
             return 
-        old = self.mp_count[number]
-        new = old - 1
-        self.mp_count[number] = new
-        self.mp_ferq[old] -=1
-        if new > 0:
-            self.mp_ferq[new]=self.mp_ferq.get(new , 0)+1
+
+        old = self.mp_count[number]       
+        new = old-1
+        self.mp_freq[old] -=1 
+
+        if self.mp_freq[old] == 0 and new == 0:            
+            del self.mp_freq[old]   
+            del self.mp_count[number]
+            return      
+        if self.mp_freq[old] == 0:
+            del self.mp_freq[old]  
+        if new == 0:
+            del self.mp_count[number]
+        else :
+            self.mp_count[number] = new                
+            self.mp_freq[new] = self.mp_freq.get(new , 0)+1 
+      
+       
+
+        
+
+
+        # old = self.mp_count[number]
+        # new = old - 1
+        # self.mp_count[number] = new
+        # self.mp_freq[old] -=1
+        # if new > 0:
+        #     self.mp_freq[new]=self.mp_freq.get(new , 0)+1
       
     def hasFrequency(self, frequency: int) -> bool:
-        return self.mp_ferq.get(frequency,0)> 0
+        return self.mp_freq.get(frequency,0)> 0
 
         
 
