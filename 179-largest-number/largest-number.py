402451a4ee4ee9064a31f680dabb0aa36131b937
Bisrat G <bisratgulelat@gmail.com>
2026-03-07 21:39:37 +0300

    Time: 7 ms (44.00%) | Memory: 12.5 MB (73.43%) - LeetSync

---
+++ 179-largest-number/largest-number.py
@@ -0,0 +1,18 @@
+from functools import cmp_to_key
+
+class Solution:
+    def largestNumber(self, nums):
+        nums = list(map(str, nums))
+
+        def compare(x, y):
+            if x + y > y + x:
+                return -1
+            elif x + y < y + x:
+                return 1
+            else:
+                return 0
+
+        nums.sort(key=cmp_to_key(compare))
+
+        result = ''.join(nums)
+        return "0" if result[0] == "0" else result
