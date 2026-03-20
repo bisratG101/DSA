4635a524bee999142395ffb69535539cdd74f5b8
CodeNerd <bisratgulelat@gmail.com>
2026-03-20 19:11:36 +0300

    Time: 0 ms (100.00%) | Memory: 12.5 MB (90.74%) - LeetSync

--- 1677-matrix-diagonal-sum/matrix-diagonal-sum.py
+++ 1677-matrix-diagonal-sum/matrix-diagonal-sum.py
@@ -4,17 +4,39 @@ class Solution(object):
         :type mat: List[List[int]]
         :rtype: int
         """
-        sum =0
-        r = len(mat[0])-1
+
+        j = len(mat)-1
+        sum = 0
         for i in range(len(mat)):
-            for j in range(len(mat[0])):
-                if j==i :
-                    sum+=mat[i][j]
-                if j+i == r:     
-                    if i==j:
-                        continue               
-                    sum+=mat[i][j]
+            sum+=mat[i][i]
+            if j==i:
+                j-=1
+                continue
+            sum+=mat[i][j]
+            j-=1
         return sum
+        
+
+
+
+
+
+
+
+
+
+
+        # sum =0
+        # r = len(mat[0])-1
+        # for i in range(len(mat)):
+        #     for j in range(len(mat[0])):
+        #         if j==i :
+        #             sum+=mat[i][j]
+        #         if j+i == r:     
+        #             if i==j:
+        #                 continue               
+        #             sum+=mat[i][j]
+        # return sum
         
                 
 
